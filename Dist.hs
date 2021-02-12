ac9bea696af625dd6c6be53a798f3adbefd0080f
Nick Smallbone <nick@smallbone.se>
2021-02-12 01:59:15 +0100

    A first attempt at multiplication (very ugly!)

--- Dist.hs
+++ Dist.hs
@@ -1,6 +1,7 @@
 module Dist where
 
-import Data.List( insert, sort )
+import Data.List( insert, sort, sortBy )
+import Data.Ord( comparing )
 
 data Dist
   = Dist
@@ -10,9 +11,84 @@ data Dist
  deriving ( Eq, Ord, Show )
 
 data Piece
-  = Line{ start :: (Double,Double), end :: (Double,Double) } -- open, >0
+  = Line{ start :: (Double,Double), end_ :: (Double,Double) } -- open, >0
   | Point{ start :: (Double,Double) }
  deriving ( Eq, Show )
+
+end :: Piece -> (Double, Double)
+end (Line _ p) = p
+end (Point p) = p
+
+value, distance :: (Double, Double) -> Double
+value = fst
+distance = snd
+
+segment :: (Double, Double) -> (Double, Double) -> [Piece]
+segment p q = segments (sortBy (comparing value) [p, q])
+
+segments :: [(Double, Double)] -> [Piece]
+segments [p] = [Point p]
+segments (p:q:ps) | value p == value q =
+  segments $ (if distance p < distance q then p else q):ps
+segments (p:q:ps) = Point p:Line p q:segments (q:ps)
+
+squareRootPieces :: Double -> Double -> [Piece]
+squareRootPieces a c =
+  segments [(a, sqrt a), (b, sqrt b), (c, sqrt c)]
+  where
+    b = sqrt(a*c)/2 + (a+c)/4 -- minimises absolute error
+
+holding :: ((Double, Double) -> (Double, Double) -> (Double, Double)) -> Piece -> Piece -> [Piece]
+holding f (Point p) (Point q) =
+  [Point (f p q)]
+holding f (Point p) (Line q1 q2) =
+  segment (f p q1) (f p q2)
+holding f (Line p1 p2) (Point q) =
+  segment (f p1 q) (f p2 q)
+holding f (Line p1 p2) (Line q1 q2) =
+  segment (f p1 q1) (f p1 q2) ++
+  segment (f p2 q1) (f p2 q2) ++
+  segment (f p1 q1) (f p2 q1) ++
+  segment (f p1 q2) (f p2 q2)
+
+distAdd :: Dist -> Dist -> Dist
+distAdd = lift2 (holding (\(x1, d1) (x2, d2) -> (x1+x2, d1+d2)))
+
+distMul :: Dist -> Dist -> Dist
+distMul = lift2 pieceMul
+  where
+    pieceMul p q =
+      squareRootPiece p q ++
+      holding (\(x1, d1) (x2, d2) -> (x1 * x2, d1 + d2)) p q
+
+    squareRootPiece l1@(Line (x1, d1) (x2, d2)) l2@(Line (y1, e1) (y2, e2))
+      | x1 == x2 || y1 == y2 || d1 == d2 || e1 == e2 = []
+      | otherwise =
+        map (scale (a*b)) (squareRootPiece' (scale (1/a) l1) (scale (1/b) l2))
+      where
+        a = (d2-d1)/(x2-x1)
+        b = (e2-e1)/(y2-y1)
+    squareRootPiece _ _ = []
+
+    squareRootPiece' (Line (x1, d1) (x2, d2)) (Line (y1, e1) (y2, e2))
+      | z1 < z2 = map (addDistance (d + e)) (squareRootPieces z1 z2)
+      | otherwise = []
+      where
+        z1 = x1 `max` y1
+        z2 = x2 `min` y2
+        d = d1 + (z1 - x1) * (d2 - d1) / (x2 - x1)
+        e = e1 + (x2 - y1) * (e2 - e1) / (y2 - y1)
+
+scale :: Double -> Piece -> Piece
+scale a (Point (x, d)) = Point (a*x, d)
+scale a (Line (x1, d1) (x2, d2))
+  | a == 0    = Point (0, d1 `min` d2)
+  | a < 0     = Line (x2*a, d2) (x1*a, d1)
+  | otherwise = Line (x1*a, d1) (x2*a, d2)
+
+addDistance :: Double -> Piece -> Piece
+addDistance a (Point (x, d)) = Point (x, a+d)
+addDistance a (Line (x1, d1) (x2, d2)) = Line (x1, d1+a) (x2, d2+a)
 
 instance Ord Piece where
   p `compare` q = (start p, mslope p) `compare` (start q, mslope q)
